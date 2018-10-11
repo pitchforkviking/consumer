92a4c51bd6d8d09bf73b87416be33f78da6f0347
Kiran Raj <M1041558@mindtree.com>
2018-10-11 15:34:12 +0530

    Add project files.

---
+++ Consumer/Controllers/ConsumerController.cs
@@ -0,0 +1,179 @@
+﻿namespace Consumer.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Newtonsoft.Json;
+    using RestSharp;
+
+    public class ApplicationUser
+    {
+        public string EmailID { get; set; }
+
+        public string Password { get; set; }
+
+    }
+
+    public class ApplicationEvent
+    {
+        public string EmailID { get; set; }
+
+        public string EventTitle { get; set; }
+
+        public string EventDate { get; set; }
+
+        public int Days { get; set; }
+
+        public bool IsCountDown { get; set; }
+
+    }
+
+    public class ApplicationEventResponse
+    {
+        public string Key { get; set; }
+        public string EventTitle { get; set; }
+
+        public int Days { get; set; }
+
+        public bool IsCountDown { get; set; }
+
+    }
+
+    public class ConsumerController : ApiController
+    {
+        private string ApiUrl = System.Configuration.ConfigurationManager.AppSettings["ApiUrl"];
+
+        [HttpPost]
+        public IHttpActionResult SignIn(ApplicationUser applicationUser)
+        {
+            try
+            {
+                RestClient restClient = new RestClient(ApiUrl);
+                RestRequest restRequest = new RestRequest("/api/Provider/SignIn", Method.POST);
+
+                restRequest.AddParameter("application/json", JsonConvert.SerializeObject(applicationUser), ParameterType.RequestBody);
+
+                IRestResponse restResponse = restClient.Execute(restRequest);
+                var responseContent = restResponse.Content;
+
+                if(restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok();
+                }
+
+                return BadRequest(responseContent);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message??exception.InnerException.Message);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult SignUp(ApplicationUser applicationUser)
+        {
+            try
+            {
+                RestClient restClient = new RestClient(ApiUrl);
+                RestRequest restRequest = new RestRequest("/api/Provider/SignUp", Method.POST);
+
+                restRequest.AddParameter("application/json", JsonConvert.SerializeObject(applicationUser), ParameterType.RequestBody);
+
+                IRestResponse restResponse = restClient.Execute(restRequest);
+                var responseContent = restResponse.Content;
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok();
+                }
+
+                return BadRequest(responseContent);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message ?? exception.InnerException.Message);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(ApplicationEvent applicationEvent)
+        {
+            try
+            {
+                RestClient restClient = new RestClient(ApiUrl);
+                RestRequest restRequest = new RestRequest("/api/Provider/AddEvent", Method.POST);
+
+                restRequest.AddParameter("application/json", JsonConvert.SerializeObject(applicationEvent), ParameterType.RequestBody);
+
+                IRestResponse restResponse = restClient.Execute(restRequest);
+                var responseContent = restResponse.Content;
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok();
+                }
+
+                return BadRequest(responseContent);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message ?? exception.InnerException.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult List(string partitionKey)
+        {
+            try
+            {
+                RestClient restClient = new RestClient(ApiUrl);
+                RestRequest restRequest = new RestRequest("/api/Provider/GetEvents", Method.GET);
+
+                restRequest.AddParameter("partitionKey", partitionKey);
+
+                IRestResponse restResponse = restClient.Execute(restRequest);
+                var responseContent = restResponse.Content;
+                var response = JsonConvert.DeserializeObject<List<ApplicationEventResponse>>(responseContent);
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok(response);
+                }
+
+                return BadRequest("No Val!!!");
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message ?? exception.InnerException.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult Delete(string partitionKey, string rowKey)
+        {
+            try
+            {
+                RestClient restClient = new RestClient(ApiUrl);
+                RestRequest restRequest = new RestRequest("/api/Provider/DeleteEvent", Method.GET);
+
+                restRequest.AddParameter("partitionKey", partitionKey);
+                restRequest.AddParameter("rowKey", rowKey);
+
+                IRestResponse restResponse = restClient.Execute(restRequest);                
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok();
+                }
+
+                return BadRequest("Failed");
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message ?? exception.InnerException.Message);
+            }
+        }
+    }
+}
---
+++ Consumer/Scripts/Consumer.js
@@ -0,0 +1,114 @@
+﻿var theCountApp = angular.module('theCountApp', []);
+
+theCountApp.controller('theCountAppController', function ($scope, $http) {
+
+    $scope.baseApiUrl = window.location.protocol + "//" + window.location.host + "/api/Consumer";
+
+    $scope.isSignedIn = false;
+    $scope.isSignIn = false;
+    $scope.isSignUp = false;
+    $scope.isAdd = false;
+
+    $scope.emailID = '';
+
+    $scope.eventList = [];
+
+
+    $scope.message = 'Make a Memory';
+
+    $scope.OpenSignIn = function () {
+        $scope.isSignedIn = false;
+        $scope.isSignIn = true;
+        $scope.isSignUp = false;
+
+        $scope.message = 'Sign In to Bring World Peace';
+    }
+
+    $scope.OpenSignUp = function () {
+        $scope.isSignedIn = false;
+        $scope.isSignIn = false;
+        $scope.isSignUp = true;
+
+        $scope.message = 'Sign Up for a Bright Future';
+    }
+
+    $scope.OpenHome = function () {
+        $scope.isSignIn = false;
+        $scope.isSignUp = false;
+        $scope.isAdd = false;
+
+        $scope.message = 'Home Sweet Home';
+    }
+
+    $scope.OpenAdd = function () {
+        $scope.isAdd = true;
+        $scope.message = 'Make a Memory';
+    }
+
+    $scope.SignIn = function (applicationUserObj) {
+        
+        $http.post($scope.baseApiUrl + '/SignIn', applicationUserObj)
+            .then(
+            function (data) {
+                $scope.isSignedIn = true;
+                $scope.emailID = applicationUserObj.EmailID;
+                $scope.List();
+                $scope.OpenHome();                
+            },
+            function (err) {
+                $scope.message = 'I\'ve Failed You';
+            })       
+    }
+
+    $scope.SignUp = function (applicationUserObj) {
+        $http.post($scope.baseApiUrl + '/SignUp', applicationUserObj)
+            .then(
+            function (data) {
+                $scope.isSignedIn = true;                
+                $scope.message = 'Signed Up';
+                $scope.emailID = applicationUserObj.EmailID;
+                $scope.OpenHome();
+            },
+            function (err) {
+                $scope.message = 'I\'ve Failed You';
+            }) 
+    }    
+
+    $scope.Add = function (applicationEventObj) {
+        applicationEventObj.EmailID = $scope.emailID;
+        $http.post($scope.baseApiUrl + '/Add', applicationEventObj)
+            .then(
+            function (data) {
+                $scope.message = 'Added';
+                $scope.List();
+                $scope.OpenHome();
+            },
+            function (err) {
+                $scope.message = 'I\'ve Failed You';
+            }) 
+    }
+
+    $scope.List = function () {
+        $http.get($scope.baseApiUrl + '/List?partitionKey=' + $scope.emailID)
+            .then(
+            function (response) {
+                $scope.eventList = response.data;
+                //$scope.message = 'Added';
+            },
+            function (err) {
+                $scope.message = 'I\'ve Failed You';
+            })
+    }
+
+    $scope.Delete = function (rowKey) {
+        $http.get($scope.baseApiUrl + '/Delete?partitionKey=' + $scope.emailID + '&rowKey=' + rowKey)
+            .then(
+            function (response) {
+                $scope.List();
+            },
+            function (err) {
+                $scope.message = 'I\'ve Failed You';
+            })
+    }
+
+});
